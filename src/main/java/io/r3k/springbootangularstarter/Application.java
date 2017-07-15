b3643f6e8b2e624d975fc801f38a71a459e82038
Richard J.D. Kolb <kata@jozijugdojo.co.za>
2017-07-15 05:58:35 +0200

    rename package to say angular

---
+++ src/main/java/io/r3k/springbootangularstarter/Application.java
@@ -0,0 +1,13 @@
+package io.r3k.springbootangularstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
